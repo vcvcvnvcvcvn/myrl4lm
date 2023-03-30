c2100b45cd491b947343b9c1e90791778bb73c0e
vcvcvnvcvcvn <gzxu98@gmail.com>
2023-03-30 20:51:01 +0100

    cor

--- rl4lms/envs/text_generation/my_metric_reward.py
+++ rl4lms/envs/text_generation/my_metric_reward.py
@@ -15,7 +15,7 @@ if not os.path.exists(REWARD_CHECKPOINT_PATH):
     os.makedirs("reward_model/rm_checkpoint", exist_ok=True)
     os.system(
         f"wget -O {REWARD_CHECKPOINT_PATH} \
-        https://huggingface.co/Jieming/Active-learning-reward-model/tree/main#:~:text=pytorch_model.bin"
+        https://huggingface.co/Jieming/Active-learning-reward-model/resolve/main/pytorch_model.bin"
     )
 #https://huggingface.co/Jieming/Active-learning-reward-model/tree/main#:~:text=pytorch_model.bin
 SFT_MODEL_PATH = "EleutherAI/gpt-neo-2.7B"
